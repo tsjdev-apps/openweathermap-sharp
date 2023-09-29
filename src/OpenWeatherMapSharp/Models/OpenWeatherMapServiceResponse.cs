ed8038574ed4dfc1aebe1116c02e6ba0404c0c58
Sebastian Jensen <apps@tsjdev-apps.de>
2023-09-29 19:24:27 +0200

    Add OpenWeatherMapServiceResponse and the HttpService.

---
+++ src/OpenWeatherMapSharp/Models/OpenWeatherMapServiceResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace OpenWeatherMapSharp.Models
+{
+    /// <summary>
+    ///     Wrapper for the OpenWeatherMapService response
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class OpenWeatherMapServiceResponse<T> where T : class
+    {
+        /// <summary>
+        ///     Indicates if the request was successful
+        /// </summary>
+        public bool IsSuccess { get; set; }
+
+        /// <summary>
+        ///     Contains the provided response object
+        /// </summary>
+        public T Response { get; set; }
+
+        /// <summary>
+        ///     Error information
+        /// </summary>
+        public string Error { get; set; }
+    }
+}
---
+++ src/OpenWeatherMapSharp/Utils/HttpService.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using OpenWeatherMapSharp.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OpenWeatherMapSharp.Utils
+{
+    internal static class HttpService
+    {
+        internal static async Task<OpenWeatherMapServiceResponse<TClass>> GetDataAsync<TClass>(string url) where TClass : class
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var json = await client.GetStringAsync(url);
+
+                    if (string.IsNullOrWhiteSpace(json))
+                    {
+                        return null;
+                    }
+
+                    return new OpenWeatherMapServiceResponse<TClass>
+                    {
+                        IsSuccess = true,
+                        Response = JsonConvert.DeserializeObject<TClass>(json)
+                    };
+                }
+                catch (Exception ex)
+                {
+                    return new OpenWeatherMapServiceResponse<TClass>
+                    {
+                        IsSuccess = false,
+                        Error = ex.Message
+                    };
+                }
+            }
+        }
+    }
+}
