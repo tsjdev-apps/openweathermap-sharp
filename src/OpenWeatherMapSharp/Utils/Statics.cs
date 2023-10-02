91a069da20d5b50b63e649dc9a5b6ee1b10f1132
Sebastian Jensen <apps@tsjdev-apps.de>
2023-10-02 09:38:16 +0200

    Extend Statics with new endpoints for geolocation.

--- src/OpenWeatherMapSharp/Utils/Statics.cs
+++ src/OpenWeatherMapSharp/Utils/Statics.cs
@@ -2,9 +2,10 @@
 {
     internal static class Statics
     {
-        private static readonly string BaseUri = "https://api.openweathermap.org/data/2.5";
-        private static readonly string WeatherBaseUri = $"{BaseUri}/weather";
-        private static readonly string ForecastBaseUri = $"{BaseUri}/forecast";
+        private static readonly string BaseUri = "https://api.openweathermap.org";
+        private static readonly string WeatherBaseUri = $"{BaseUri}/data/2.5/weather";
+        private static readonly string ForecastBaseUri = $"{BaseUri}/data/2.5/forecast";
+        private static readonly string GeocodeBaseUri = $"{BaseUri}/geo/1.0";
 
         public static readonly string WeatherCoordinatesUri
             = WeatherBaseUri + "?lat={0}&lon={1}&units={2}&lang={3}&appid={4}";
@@ -23,5 +24,14 @@
 
         public static readonly string ForecastIdUri
             = ForecastBaseUri + "?id={0}&units={1}&lang={2}&appid={3}";
+
+        public static readonly string GeocodeLocationUri
+            = GeocodeBaseUri + "/direct?q={0}&limit={1}&appid={2}";
+
+        public static readonly string GeocodeZipUri
+            = GeocodeBaseUri + "/zip?zip={0}&appid={1}";
+
+        public static readonly string GeocodeReverseUri
+            = GeocodeBaseUri + "/reverse?lat={0}&lon={1}&limit={2}&appid={3}";
     }
 }
